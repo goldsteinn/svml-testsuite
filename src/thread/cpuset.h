941586d82e18f74b51c416c7a7b36c659dcd000d
Noah Goldstein <goldstein.w.n@gmail.com>
2022-05-04 10:12:49 -0500

    Add thread helpers

--- src/thread/cpuset.h
+++ src/thread/cpuset.h
@@ -7,14 +7,8 @@
 
 #include "util/inline-math.h"
 
-#ifdef SYS_UTIL_SUPPORT_N_CPUS
-typedef cpu_set_t cpuset_t;
+#ifdef CPU_SET_OPTIMIZED
 
-#define cset_set   CPU_SET
-#define cset_isset CPU_ISSET
-#define cset_count CPU_COUNT
-
-#else
 typedef uint64_t cpuset_t;
 
 static void
@@ -23,14 +17,23 @@ NONNULL(2) cset_set(uint32_t cpu, cpuset_t * cset) {
 }
 
 static uint64_t
-NONNULL(2) cset_isset(uint32_t cpu, const cpuset_t * cset) {
+NONNULL(2) cset_isset(uint32_t cpu, cpuset_t const * cset) {
     return (*cset & (1UL << cpu));
 }
 
 static uint32_t
-NONNULL(1) cset_count(const cpuset_t * cset) {
+NONNULL(1) cset_count(cpuset_t const * cset) {
     return __builtin_popcountll(*cset);
 }
+
+
+#else
+
+typedef cpu_set_t cpuset_t;
+
+#define cset_set   CPU_SET
+#define cset_isset CPU_ISSET
+#define cset_count CPU_COUNT
 
 #endif
 
@@ -38,5 +41,6 @@ static void
 NONNULL(1) cset_zero(cpuset_t * cset) {
     __builtin_memset(cset, 0, sizeof(cpuset_t));
 }
+
 
 #endif
--- src/thread/thread-common.h
+++ src/thread/thread-common.h
@@ -1,11 +1,16 @@
 #ifndef _SRC__THREAD__THREAD_COMMON_H_
 #define _SRC__THREAD__THREAD_COMMON_H_
 
-#include "util/types.h"
+#include "thread/cpuset.h"
 
-#define WRITE_ONCE(var, val) (*((volatile get_type(val) *)(&(var))) = (val))
-#define READ_ONCE(var)       (*((volatile get_type(var) *)(&(var))))
-#define CASTV(var)           CAST(volatile get_type(var), var)
-
+#ifdef WITH_THREAD
+#include "thread/barrier.h"
+#include "thread/mutex.h"
+#include "thread/spinlock.h"
+#include "thread/thread.h"
+#else
+#warning                                                                       \
+    "Including thread headers with threading disabled. Maybe rebuild with 'WITH_THREAD'"
+#endif
 
 #endif
