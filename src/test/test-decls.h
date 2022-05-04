9cbb3a59e5b6e4091068fdb8de7886e0e43afccd
Noah Goldstein <goldstein.w.n@gmail.com>
2022-05-04 10:15:02 -0500

    Add all new tests to list

--- src/test/test-decls.h
+++ src/test/test-decls.h
@@ -2,7 +2,9 @@
 #define _SRC__TEST__TEST_DECLS_H_
 
 /* Append all here. */
-#define TESTS test_p2, test_log2, test_random, test_vdso, test_max_min
+#define TESTS                                                                  \
+    test_p2, test_log2, test_random, test_vdso, test_max_min, test_sort,       \
+        test_types, test_stats
 
 
 #endif
